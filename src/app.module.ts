92a7bd987d925b16df4262e81dc13918a3f13dbb
GarciaNJ <nicolas.jg.njg@gmail.com>
2024-03-26 18:02:13 -0300

    feat:created postgresConfigService and configured in appModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,20 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PostgresConfigService } from './config/postgres.config.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule,
+    ConfigModule.forRoot({
+      isGlobal:true
+    }),
+    TypeOrmModule.forRootAsync({
+      useClass:PostgresConfigService,
+      inject:[PostgresConfigService]
+    })  
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
