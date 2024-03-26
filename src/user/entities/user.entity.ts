f033ef29d9a0d38770be08a1698d9ee82212d070
GarciaNJ <nicolas.jg.njg@gmail.com>
2024-03-26 18:12:09 -0300

    feat: created User entity

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,22 @@
-export class User {}
+import { AbstractEntity } from "src/config/abstract/abstract.entity";
+import { Column, Entity } from "typeorm";
+
+@Entity({ name: 'users' })      //Id extendido através de Abstract
+export class User extends AbstractEntity<User>{
+
+    @Column({
+        name: 'nome',
+        type:'varchar',
+        length: 100,
+        nullable: false
+    })
+    email: string;
+
+    @Column({
+        name: 'password',
+        type:'varchar',
+        length: 100,
+        nullable: false
+    })
+    password: string;
+}
