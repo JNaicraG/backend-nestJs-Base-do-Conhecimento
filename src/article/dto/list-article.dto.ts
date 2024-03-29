c15a4c5306907c190eb11b29b38a3f1564bced6a
GarciaNJ <nicolas.jg.njg@gmail.com>
2024-03-29 20:36:41 -0300

    feat: created list articles dto

---
+++ src/article/dto/list-article.dto.ts
@@ -0,0 +1,13 @@
+export class ListArticleDto {
+    constructor(
+        readonly id:number;
+        readonly userId:number;
+        readonly categoryId:number;
+        readonly name:string;
+        readonly content:string;
+        readonly description:string;
+        readonly url:string;
+    ){}
+}
+
+         
