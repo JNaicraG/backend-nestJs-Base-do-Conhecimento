6646a75ec087b3036698cc9f248597c60d649d40
GarciaNJ <nicolas.jg.njg@gmail.com>
2024-03-29 20:10:59 -0300

    feat: created category controller

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -3,7 +3,7 @@ import { CategoryService } from './category.service';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
 
-@Controller('category')
+@Controller('categories')
 export class CategoryController {
   constructor(private readonly categoryService: CategoryService) {}
 
@@ -15,6 +15,11 @@ export class CategoryController {
   @Get()
   findAll() {
     return this.categoryService.findAll();
+  }
+
+  @Get('/tree')
+  findTrees() {
+    return this.categoryService.findTrees();
   }
 
   @Get(':id')
