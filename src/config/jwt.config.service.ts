1c303a0c353e16247afb96edcafd7eb5a3fa4225
GarciaNJ <nicolas.jg.njg@gmail.com>
2024-04-02 00:24:34 -0300

    feat: created jwtService configuration

---
+++ src/config/jwt.config.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { JwtModuleOptions, JwtOptionsFactory } from "@nestjs/jwt";
+
+@Injectable()
+export class JwtConfigService implements JwtOptionsFactory{
+
+    constructor(
+        private configService:ConfigService
+    ){}
+
+    createJwtOptions(): JwtModuleOptions {
+        return {
+            global:true,
+            secret: this.configService.get<string>('AUTH_SECRET'),
+            signOptions:{expiresIn:'3d'}
+            //https://stackoverflow.com/questions/72314255/jwt-not-expiring-in-nestjs-application-even-after-setting-expiresin-value
+            //https://docs.nestjs.com/security/authentication#implementing-the-sign-in-endpoint
+        }
+    }
+
+}
