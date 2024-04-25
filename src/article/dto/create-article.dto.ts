137d6b4d0cc090d8983deab8fd9f0e090c888c5f
GarciaNJ <nicolas.jg.njg@gmail.com>
2024-04-25 13:38:25 -0300

    feat(article):implemented swagger

--- src/article/dto/create-article.dto.ts
+++ src/article/dto/create-article.dto.ts
@@ -1,18 +1,40 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsNotEmpty, IsNumber, IsString } from "class-validator";
 
 export class CreateArticleDto {
+    @ApiProperty({
+        description:'Id do usuário quem criou este artigo',
+        example:1
+    })
     @IsNotEmpty()
     @IsNumber()
     userId:number;
 
+    /**
+     * Categoria a qual pertence este artigo
+     * @example:2
+     */
     @IsNotEmpty()
     @IsNumber()
     categoryId:number;
 
+    /**
+     * Nome que foi dado ao artigo a ser criado
+     * @example:Como criar um projeto em NodeJS
+     */
     @IsNotEmpty()
     @IsString()
     name:string;
     
+    @ApiProperty({
+        description:'Conteúdo do artigo a ser criado',
+        example:'O conteúdo pode ser grande e verboso, normalmente explicando algum conceito ou semelhante',
+        enum:{
+            do:'Cabral',
+            po:"Xacal",
+            no:"Papal"
+        }
+    })
     @IsNotEmpty()
     @IsString()
     content:string;
