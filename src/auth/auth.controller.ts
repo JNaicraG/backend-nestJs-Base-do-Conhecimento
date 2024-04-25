31f54bed2c253d787fde1333618a08cad0b8d6a9
GarciaNJ <nicolas.jg.njg@gmail.com>
2024-04-25 13:40:23 -0300

    feat(auth):created and implemented auth and login auth

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,9 @@
 import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SignInDto } from './dto/signin.dto';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('auth')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
@@ -12,4 +14,5 @@ export class AuthController {
   signIn(@Body() signInDto: SignInDto){
     return this.authService.signIn(signInDto);
   }
+
 }
